29ee143b104dc129abb4212c3a33fa47af059748
jr635 <70586888+jr635@users.noreply.github.com>
2020-09-24 16:27:38 -0400

    Delete config.php

--- lib/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-$dbhost = "localhost";//this should be fine for all but NJIT, which uses "sql1.njit.edu" or "sql2.njit.edu" or "sql3.njit.edu"
-$dbuser = "jr635";//your ucid since we follow this pattern
-$dbpass = "YMCJG1QWiCghGZYs";//database password generated or chosen per respective steps
-$dbdatabase = "jr635";//your ucid since we follow this pattern
-?>
